1536817a6033eb420e14cbf53f617a8d111d8c80
HorseBadorties <no@thanks.com>
2017-08-25 10:23:16 +0200

    SVGSalamanderTest

--- src/main/kotlin/de/toto/crt/game/gui/javafx/GuiTest.kt
+++ src/main/kotlin/de/toto/crt/game/gui/javafx/GuiTest.kt
@@ -4,7 +4,6 @@ import de.toto.crt.game.rules.FEN_STARTPOSITION
 import de.toto.crt.game.rules.fromFEN
 import javafx.application.Application
 import javafx.scene.Scene
-import javafx.scene.control.Button
 import javafx.scene.layout.BorderPane
 import javafx.stage.Stage
 
@@ -17,11 +16,10 @@ class App: Application() {
     override fun start(stage: Stage?) {
         stage?.title = "JavaFX-Tester"
         val border = BorderPane()
-        border.top = Button("do somthing")
         val board = ChessBoard()
         board.setPosition(fromFEN(FEN_STARTPOSITION))
         border.center = board
-        stage?.scene = Scene(border, 600.0, 600.0)
+        stage?.scene = Scene(border, 800.0, 800.0)
         stage?.show()
     }
 
